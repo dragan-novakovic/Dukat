c3786e3d0f202ebda56171bed39cd3924860cd3e
dragan1810 <d.novakovic1810@yahoo.com>
2021-02-07 22:23:47 +0100

    qqq

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+struct DukatChain {
+    chain: Vec<Dukat>,
+}
+
+struct Dukat {
+    index: i32,
+    hash: i32,
+    time: i32,
+    transactions: Vec<Transaction>,
+    prevHash: Option<i32>,
+}
+
+struct Transaction {
+    sender: String,
+    reciever: String,
+    amount: i64,
+    time: i32,
+    hash: i32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
