c32507ff3e893b93907bef80752db7ce4d678263
dragan1810 <d.novakovic1810@yahoo.com>
2021-02-08 21:40:39 +0100

    qqq

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 struct DukatChain {
     chain: Vec<Dukat>,
+    pending_transactions: Vec<Transaction>,
+    // difficulty
+    // minerReward
+    // blockSize
+    // self.nodes = set() ?
+}
+
+impl DukatChain {
+    fn add_block() {}
+    fn get_last_block() {}
+    fn add_transaction() {}
+    // register node
+    // resolve conficts
+    // minePending
+    // addGenesisBlock
+    // isValid Chain
+    // genereate KEys
+    // chainJSON encode
 }
 
 struct Dukat {
@@ -7,7 +25,12 @@ struct Dukat {
     hash: i32,
     time: i32,
     transactions: Vec<Transaction>,
-    prevHash: Option<i32>,
+    prev_hash: Option<i32>,
+}
+
+impl Dukat {
+    fn calculate_hash() {}
+    fn mine_block() {}
 }
 
 struct Transaction {
@@ -16,6 +39,10 @@ struct Transaction {
     amount: i64,
     time: i32,
     hash: i32,
+}
+
+impl Transaction {
+    fn sign_transactions() {}
 }
 
 fn main() {
