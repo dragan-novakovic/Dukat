a3f25bc648a2447b6a55442151955f80d51de2d4
dragan1810 <d.novakovic1810@yahoo.com>
2021-02-08 21:59:17 +0100

    qqq

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,29 @@ impl DukatChain {
     fn add_block() {}
     fn get_last_block() {}
     fn add_transaction() {}
+    fn add_genesis_block(&mut self) {
+        let mut transactions: Vec<Transaction> = vec![];
+
+        let genesis_transaction = Transaction {
+            sender: String::from("Root"),
+            reciever: String::from("Zero"),
+            amount: 10,
+            hash: 111,
+            time: 1,
+        };
+
+        transactions.push(genesis_transaction);
+
+        let genesis_block = Dukat {
+            index: 0,
+            hash: 11,
+            time: 1,
+            transactions,
+            prev_hash: None,
+        };
+
+        self.chain.push(genesis_block)
+    }
     // register node
     // resolve conficts
     // minePending
