c409411f1b18bb779f972b962d659e6fc0dbfd0c
dragan1810 <d.novakovic1810@yahoo.com>
2021-02-14 22:03:11 +0100

    sitno nesto

--- src/implementation/dukat.rs
+++ src/implementation/dukat.rs
@@ -2,7 +2,7 @@ use chrono::prelude::*;
 use std::fmt::{self, Debug, Formatter};
 
 use crate::implementation::transaction::Transaction;
-use crate::utils::hashable::{u128_bytes, Hashable};
+use crate::utils::hashable::{difficulty_bytes_as_u128, u128_bytes, Hashable};
 
 pub struct Dukat {
     pub index: u128,
@@ -11,6 +11,8 @@ pub struct Dukat {
     pub transactions: Vec<Transaction>,
     pub prev_hash: Vec<u8>,
     pub payload: String,
+    pub difficulty: u128,
+    pub nonce: u64,
 }
 
 impl Dukat {
@@ -19,6 +21,8 @@ impl Dukat {
         transactions: Vec<Transaction>,
         prev_hash: Vec<u8>,
         payload: String,
+        difficulty: u128,
+        nonce: u64,
     ) -> Self {
         let mut block = Dukat {
             index,
@@ -27,12 +31,28 @@ impl Dukat {
             hash: vec![0; 32],
             prev_hash,
             payload,
+            difficulty,
+            nonce,
         };
 
         block.hash = Dukat::hash(&block);
         block
     }
-    pub fn _mine_block() {}
+    pub fn mine_block(&mut self) {
+        for nonce_attempt in 0..(u64::max_value()) {
+            self.nonce = nonce_attempt;
+            let hash = self.hash();
+
+            if Dukat::check_difficulty(&hash, self.difficulty) {
+                self.hash = hash;
+                return;
+            }
+        }
+    }
+
+    pub fn check_difficulty(hash: &Vec<u8>, difficulty: u128) -> bool {
+        difficulty > difficulty_bytes_as_u128(&hash)
+    }
 }
 
 impl Hashable for Dukat {
@@ -42,6 +62,7 @@ impl Hashable for Dukat {
         bytes.extend(&u128_bytes(&self.index));
         bytes.extend(&self.time.timestamp().to_be_bytes());
         bytes.extend(self.payload.as_bytes());
+        bytes.extend(&u128_bytes(&self.difficulty));
 
         bytes
     }
@@ -53,10 +74,13 @@ impl Debug for Dukat {
             f,
             r#"Block[{}]: {},
             timestamp: {},
-            payload: {}"#,
+            nonce: {},
+            payload: {}
+            "#,
             &self.index,
             hex::encode(&self.hash),
             &self.time,
+            &self.nonce,
             &self.payload
         )
     }
--- src/implementation/dukatchain.rs
+++ src/implementation/dukatchain.rs
@@ -3,6 +3,7 @@ use std::fmt::{self, Debug, Formatter};
 
 use crate::implementation::dukat::Dukat;
 use crate::implementation::transaction::Transaction;
+use crate::utils::hashable::Hashable;
 
 pub struct DukatChain {
     pub chain: Vec<Dukat>,
@@ -27,11 +28,45 @@ impl DukatChain {
         let genesis_transaction = Transaction::new("Root".to_owned(), "First".to_owned(), 1);
         transactions.push(genesis_transaction);
 
-        let genesis_block = Dukat::new(0, vec![], vec![0; 32], "GENESIS".to_owned());
+        let genesis_block = Dukat::new(0, vec![], vec![0; 32], "GENESIS".to_owned(), 0, 0);
         self.chain.push(genesis_block)
     }
 
-    pub fn _add_block() {}
+    pub fn add_block(block: Dukat) {
+        //last hash
+
+        //verify
+    }
+
+    pub fn verify(&self) -> bool {
+        for (i, block) in self.chain.iter().enumerate() {
+            if block.index != i as u128 {
+                println!("Index mismatch {} != {}", &block.index, &i);
+                return false;
+            } else if !Dukat::check_difficulty(&block.hash(), block.difficulty) {
+                println!("Difficulty Fail");
+                return false;
+            } else if i == 0 {
+                // not genesis
+                let prev_block = &self.chain[i - 1];
+                if block.time <= prev_block.time {
+                    println!("Time Failed");
+                    return false;
+                } else if block.prev_hash != prev_block.prev_hash {
+                    println!("Hash mismatch");
+                    return false;
+                }
+            } else {
+                //genesis
+                if block.prev_hash != vec![0; 32] {}
+                println!("Gensis block invalid");
+                return false;
+            }
+        }
+
+        true
+    }
+
     pub fn _add_pending_transaction() {}
 
     // register node
