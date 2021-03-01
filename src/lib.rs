e6023d4a07721285c9ff96705d5aa59d1e1c022c
dragan1810 <d.novakovic1810@yahoo.com>
2021-03-01 22:09:41 +0100

    ipak wasm

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,24 @@ mod utils;
 use implementation::dukat::Dukat;
 use implementation::dukatchain::DukatChain;
 use implementation::transaction::Transaction;
-use neon::prelude::*;
 use std::convert::TryInto;
 
-declare_types! {
-    /// JS class wrapping Employee records.
-    pub class JsDukatChain for DukatChain {
-        init(mut cx) {
-            Ok(DukatChain::new())
-        }
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: String);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: String) {
+    unsafe {
+        alert(format!("Hello, {}!", name));
     }
 }
-// Export the class
-register_module!(mut m, {
-    // <JsEmployee> tells neon what class we are exporting
-    // "Employee" is the name of the export that the class is exported as
-    m.export_class::<JsDukatChain>("DukatChain")?;
-    Ok(())
-});
 
-/*
-
-
-
-fn main() {
+pub fn init() {
+    let mut blockchain = DukatChain::new();
 
     let first_transaction = Transaction::new("Player1".to_owned(), "Player2".to_owned(), 100);
     let second_transaction = Transaction::new("Player2".to_owned(), "Player3".to_owned(), 150);
@@ -38,7 +32,7 @@ fn main() {
         vec![first_transaction, second_transaction],
         blockchain.chain.last().unwrap().hash.clone(),
         "First Interaction".to_owned(),
-        difficulty,
+        Dukat::default_difficulty(),
     );
 
     // Mine it
@@ -49,8 +43,3 @@ fn main() {
 
     dbg!(blockchain);
 }
-
-
-
-
-*/
